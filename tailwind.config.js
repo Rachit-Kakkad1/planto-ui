cc5bf5bdbd2885a922c19da4185219b5c040b539
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-05-22 13:15:22 +0530

    Initialize Vite project structure

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	content: [
+		"./index.html", 
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				}
+			},
+			animation: {
+				'fade-in': 'fadeIn 0.8s ease-in-out',
+			}
+		},
+	},
+	plugins: [],
+}
