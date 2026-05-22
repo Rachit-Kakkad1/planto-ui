4b9eb9e1fd2542c8f930b22576602c5d6f329ad0
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-05-22 13:42:10 +0530

    Add shared layout and section containers

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.5, // Even smoother and more cinematic
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
